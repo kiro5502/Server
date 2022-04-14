f17e745ec7c5df94efde8e2899da20fe9594758c
Kyuhyeon Han <35136024+kahmnkk@users.noreply.github.com>
2022-04-15 02:01:04 +0900

    add. example code
    - controller & DTO

--- src/main/java/com/ajou/mse/magicaduel/server/controller/HelloController.java
+++ src/main/java/com/ajou/mse/magicaduel/server/controller/HelloController.java
@@ -1,7 +1,10 @@
 package com.ajou.mse.magicaduel.server.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ajou.mse.magicaduel.server.dto.HelloResponseDto;
 
 @RestController
 public class HelloController {
@@ -9,6 +12,11 @@ public class HelloController {
 	@GetMapping("/")
 	public String hello() {
 		return "hello";
+	}
+
+	@GetMapping("/dto")
+	public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+		return new HelloResponseDto(name, amount);
 	}
 
 }
---
+++ src/test/java/com/ajou/mse/magicaduel/server/dto/HelloResponseDtoTest.java
@@ -0,0 +1,23 @@
+package com.ajou.mse.magicaduel.server.dto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class HelloResponseDtoTest {
+
+	@Test
+	public void 롬복_기능_테스트() {
+		// given
+		String name = "test";
+		int amount = 10;
+
+		// when
+		HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+		// then
+		assertThat(dto.getName()).isEqualTo(name);
+		assertThat(dto.getAmount()).isEqualTo(amount);
+	}
+
+}
