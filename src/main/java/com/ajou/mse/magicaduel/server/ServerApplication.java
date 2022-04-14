19fb7ab0fe38f00258d57862f4e6906e5a334051
Kyuhyeon Han <35136024+kahmnkk@users.noreply.github.com>
2022-04-14 21:36:00 +0900

    add. proj

---
+++ src/main/java/com/ajou/mse/magicaduel/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.ajou.mse.magicaduel.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
