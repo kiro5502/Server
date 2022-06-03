7f00d2c8bf22564a3112057afb31f6be78fa7b61
Kyuhyeon Han <35136024+kahmnkk@users.noreply.github.com>
2022-06-03 12:04:01 +0900

    fix. battle/start API to POST

--- src/main/java/com/ajou/mse/magicaduel/server/controller/BattleController.java
+++ src/main/java/com/ajou/mse/magicaduel/server/controller/BattleController.java
@@ -1,6 +1,5 @@
 package com.ajou.mse.magicaduel.server.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +18,7 @@ public class BattleController {
 
   private final BattleService battleService;
 
-  @GetMapping("start")
+  @PostMapping("start")
   public ResultResponseDto start() {
     return battleService.start();
   }
