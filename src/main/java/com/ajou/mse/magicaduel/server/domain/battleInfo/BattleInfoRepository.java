442b95c98eaddebd72899a2b771ded84e12cce8e
Kyuhyeon Han <35136024+kahmnkk@users.noreply.github.com>
2022-06-06 19:03:18 +0900

    del. BattleInfo directory
    - fix conflict in Github & VSCode

--- src/main/java/com/ajou/mse/magicaduel/server/domain/battleInfo/BattleInfoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ajou.mse.magicaduel.server.domain.battleinfo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BattleInfoRepository extends JpaRepository<BattleInfo, Long> {
-}
